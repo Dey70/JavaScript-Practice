d421467c313152f7afe0091ce3afdd8bb590b793
Rajdeep Dey <rajdeep.x70@outlook.com>
2025-07-16 23:18:55 +0530

    add compare.js

---
+++ day1/compare.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 2;
+let num3 = 10;
+
+console.log("Comparing numbers:", num1, num2, num3);
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 less than num3?", num1 < num3);
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+console.log("Is num1 greater than or equal to num2?", num1 >= num2);
+console.log("Is num1 less than or equal to num3?", num1 <= num3);
