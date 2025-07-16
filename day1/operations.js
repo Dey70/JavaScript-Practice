4b03f237255963d032431e1e6a5f192b78c08c09
Rajdeep Dey <rajdeep.x70@outlook.com>
2025-07-16 22:16:49 +0530

    add few lines

---
+++ day1/operations.js
@@ -0,0 +1,17 @@
+let add = 5+4
+let subtract = 10-3
+let multiply = 7*6
+let divide = 20/4
+let modulus = 10 % 3
+let exponent = 2 ** 3
+let increment = 5
+let decrement = 10
+
+console.log("Addition:", add);
+console.log("Subtraction:", subtract);
+console.log("Multiplication:", multiply);
+console.log("Division:", divide);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponent);
+console.log("Increment:", ++increment);
+console.log("Decrement:", --decrement);
