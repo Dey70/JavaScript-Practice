9247eab84bcee9df8b803e1dd0fdc9a3264678a5
Rajdeep Dey <rajdeep.x70@outlook.com>
2025-07-17 10:24:51 +0530

    learn non-primitive types

---
+++ day2/non-primitive.js
@@ -0,0 +1,13 @@
+const person = {
+  name: "Rajdeep",
+  "last name": "Dey",
+  age: 20,
+  isStudent: true,
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person["last name"]);
+
+person.name = "Raj";
+console.log(person);
