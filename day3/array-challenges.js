b3915d3baf55d17bc959a825e7dbed6c90637bfe
Rajdeep Dey <rajdeep.x70@outlook.com>
2025-07-17 11:40:56 +0530

    practice basiccs

---
+++ day3/array-challenges.js
@@ -0,0 +1,12 @@
+let mixed = ["Raj", 20, true];
+console.log(mixed);
+
+console.log(mixed[0]); 
+console.log(mixed[1]);
+console.log(mixed[2]);
+
+mixed[1] = 25; // Update the second element
+console.log(mixed[1]); // Output: 25
+console.log(mixed);
+
+console.log(mixed.length);
