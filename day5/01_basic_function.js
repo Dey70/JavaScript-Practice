b16cb7ecb9f075f57f40c527210a9b9d87e640b8
RAJDEEP DEY <rajdeep.x70@outlook.com>
2025-08-03 13:44:43 +0530

    solve q2 (01_basic_function.js)

--- day5/01_basic_function.js
+++ day5/01_basic_function.js
@@ -8,3 +8,17 @@ function makeTea(typeOfTea) {
 
 let teaOrder = makeTea("green tea");
 console.log(teaOrder);
+
+/*  
+2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`.  
+Call `confirmOrder` from within `orderTea` and return the result.
+*/ 
+
+function orderTea(teaType) {
+  function confirmOrder() {
+    return `Order confirmed for ${teaType}`;
+  }
+  return confirmOrder();
+}
+let order = orderTea("Chai");
+console.log(order);
