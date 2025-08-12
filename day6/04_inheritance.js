32bbe85c1c7a476922c91ef926b4e4a86c3f07fd
RAJDEEP DEY <rajdeep.x70@outlook.com>
2025-08-13 00:31:30 +0530

    Learn Inheritance

---
+++ day6/04_inheritance.js
@@ -0,0 +1,29 @@
+class Robot {
+  constructor(name, brand, year) {
+    this.name = name;
+    this.brand = brand;
+    this.year = year;
+  }
+  info() {
+    return `This robot, ${this.name}, was manufactured in ${this.year} by ${this.brand}`;
+  }
+}
+
+let robo1 = new Robot("X", "Y", 2000);
+console.log(robo1.name);
+console.log(robo1.year);
+console.log(robo1.info());
+
+class AI extends Robot {
+  constructor(name, brand, year, IQ) {
+    super(name, brand, year);
+    this.IQ = IQ;
+  }
+  anaylze() {
+    return `${this.name} is analyzing with intelligence level ${this.IQ}`;
+  }
+}
+
+let robo2 = new AI("X1", "Y1", 2005, 210);
+console.log(robo2.IQ);
+console.log(robo2.anaylze());
